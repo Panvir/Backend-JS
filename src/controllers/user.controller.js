c7452753ba40fa952c413e95ef293765ce3c3686
Panvir_Singh <panvirsingh2002@gmail.com>
2025-04-09 16:01:14 +0530

    userchannelProfile controller written

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -4,6 +4,7 @@ import {User} from "../models/user.model.js"
 import {uploadonCloudinary} from '../utils/cloudinary.js'
 import { ApiResponse } from "../utils/ApiResponse.js";
 import jwt from "jsonwebtoken"
+import { channel, subscribe } from "diagnostics_channel";
 
 const generateAccessAndRefreshTokens= async(userId)=>{
   try{
@@ -325,6 +326,81 @@ const updateUserCoverImage=asyncHandler(async(req,res)=>{
 
   return res.status(200).json(new ApiResponse(200,user,"Cover image updated successfully"))
 })
+// <-----------------------------getUserChannelprofile------------->>>>>>>>>>>>>
+const getUserChannelProfile=asyncHandler(async(req,res) => {
+  //hume channel tak jaen ka url milega params te
+  const {username}=req.params //url to milda data params nl
+  if(!username?.trim())
+  {
+    throw new ApiError(400,"username is missing")
+  }
+
+ //hum aggregation pipleine nl match krde 
+const channel=await User.aggregate([
+  {
+    $match:{
+      username:username?.toLowerCase()
+    }
+  },
+  {
+    $lookup:{
+      from: "subscriptions", //ethe Subscription nhi ayega kyyoki mongo lowercase ch krd and plural ch rakhda naam
+      localField:"_id",
+      foreignField:"channel",
+      as:"subscribers"
+    }
+  },
+  {
+    $lookup:{
+      from: "subscriptions", //ethe Subscription nhi ayega kyyoki mongo lowercase ch krd and plural ch rakhda naam
+      localField:"_id",
+      foreignField:"subscriber",
+      as:"subscribedTo"
+    }
+  },
+  {
+    $addFields:{//ehde nl fiedla add hundi additional
+      subscribersCount:{
+        $size:"$subscribers" //$tn aya subscriber agge kyoki hun oh field bn chuka ha ute
+      },
+      channelsSubscribedToCount:{
+        $size:"$subscribedTo"
+      },
+      isSubscribed:{
+        $cond :{
+          if:{$in:[req.user?._id,"$subscribers.subscriber"]},
+          then: true,
+          else:false
+
+        }
+      }
+    }
+  },
+  {
+    $project:{
+      fullName:1,
+      username:1,
+      subscribersCount:1,
+      channelsSubscribedToCount:1,
+      isSubscribed:1,
+      avatar:1,
+      coverImage:1,
+      email:1
+
+
+    }
+  }
+])
+//return krake dekhea kro ki anda output
+
+// agar channel hi nhi hai 
+if(!channel?.length){
+  throw new ApiError(404,"Channel does not exist")
+}
+
+return res.status(200).json(new ApiResponse(200,channel[0],"User channel fetched successfully"))
+})
+
 export { registerUser,
           loginUser,
           logoutUser,refreshAccessToken,
