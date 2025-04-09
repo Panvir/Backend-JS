3db107c7618631cb4c997e170d92494f287f725b
Panvir_Singh <panvirsingh2002@gmail.com>
2025-04-09 11:54:18 +0530

    add user account management updations controllers

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -237,9 +237,101 @@ const loginUser= asyncHandler(async (req,res)=>{
       throw new ApiError(401,error?.message || "Invalid refresh token")
     }
   })
+
+  // <---------------------ab chnage user pssword da controller likjde a------------------------->
+const changeCurrentPassword = asyncHandler(async(req,res)=>{
+  const {oldPassword,newPassword} = req.body
+//pehla user lbi tnhi pass verify hou also gl hai password v ohi change kru jeda login hou already
+const user = User.findById(req.user?._id)
+const isPasswordCorrect= await user.isPasswordCorrect(oldPassword)
+
+if(!isPasswordCorrect){
+  throw new ApiError(400,"Invalid old password")
+}
+
+//new set kro
+user.password= newPassword // ethe ape hash hoega kyoki method dekho model ch jake user da pre lgyaa hai ssave lyi so hash ape hoega
+await user.save({validateBeforeSave:false}) 
+
+return res.status(200)
+.json(new ApiResponse(200,{},"Password changed successfully"))
+})
+
+//<-------------------------------getCurrentUser------------------------------------------------------->>>>>>>>>>
+const getCurrentUser=asyncHandler(async(req,res)=>{
+  return res.status(200).json(200,req.user,"current user fetched successfully")//req.user ape chlega kyoki auth vele jo middleware chleya hai ohch req.user ch current user hai because in routes verify jwt runs in thath midlerware we had alread putter req.user - user
+})
+//<--------------------updateAccountDetails-------------->
+const updateAccountDetails=asyncHandler(async(req,res)=>{
+  const {fullName,email}=req.body
+  if(!fullName || !email){
+    throw new ApiError(400,"All fields are required")
+  }
+
+ const user= User.findByIdAndUpdate(req.user?._id,{$set:{fullName:fullName,email:email}},{new:true}).select("-password")
+ return res.status(200)
+ .json(new ApiResponse(200,user,"Account detail updated successfully"))
+})
+
+//<<<<<<<<<<<<<<<<<<<<<<<<-------------------------updateUserAvatar-------------------------->>>>>>>>>>>>>
+const updateUserAvatar=asyncHandler(async(req,res)=>{
+  const avatarLocalPath=req.file?.path
+
+  if(!avatarLocalPath){
+    throw new ApiError(400,"Avatar file is missing")
+  }
+
+  const avatar= await uploadonCloudinary(avatarLocalPath)
+
+  if(!avatar.url){
+    throw new ApiError(400,"Error while uploading new avatar")
+  }
+
+  const user=await User.findByIdAndUpdate(
+    req.user?._id,
+    {
+      $set:{
+        avatar:avatar.url
+      }
+    },
+    {new:true}
+  ).select("-password")
+  return res.status(200).json(new ApiResponse(200,user,"avatar image updated successfully"))
+})
+
+//<---------------------updateusercoverimage--------------------------------->>>>>>>>>>>>>>>>
+const updateUserCoverImage=asyncHandler(async(req,res)=>{
+  const coverImageLocalPath=req.file?.path
+
+  if(!coverImageLocalPath){
+    throw new ApiError(400,"cover image file is missing")
+  }
+
+  const coverImage= await uploadonCloudinary(coverImageLocalPath)
+
+  if(!coverImage.url){
+    throw new ApiError(400,"Error while uploading new avatar")
+  }
+
+  const user=await User.findByIdAndUpdate(
+    req.user?._id,
+    {
+      $set:{
+        coverImage:coverImage.url
+      }
+    },
+    {new:true}
+  ).select("-password")
+
+  return res.status(200).json(new ApiResponse(200,user,"Cover image updated successfully"))
+})
 export { registerUser,
           loginUser,
-          logoutUser,refreshAccessToken
+          logoutUser,refreshAccessToken,
+          getCurrentUser,
+          changeCurrentPassword,updateAccountDetails,
+          updateUserAvatar,
+          updateUserCoverImage
  };
 
  // Concept	Explanation
